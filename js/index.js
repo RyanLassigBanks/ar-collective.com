aa829b7d963b9a6facd4502dcf9c96856049389a
Ryan Banks <lass0067@umn.edu>
2016-09-12 23:29:51 -0500

    never forget let

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
 let events = document.getElementsByClassName('event_always_visable')
 
-for (eventObj of events) {
+for (let eventObj of events) {
    eventObj.addEventListener('click', () => {
       let description = eventObj.nextElementSibling
-      let childCount = description.childrenElementCount
-      console.log(description.children.length)
       if (!description.children.length) {
          let p = document.createElement("p")
-         let text = document.createTextNode(`Be there <br>
-                        For the rap battle! <br>
-                        It's going to be <br>
-                        Interspectacular <br>
-                        Fantabulous <br>
-                        It'll blow your socks off! <br>
-                        It's one of those things <br>
-                        Where machine versus man <br>
-                        Man versus woman <br>
-                        Woman versus your mother <br>
-                        Be there! <br>
-                        Intergalactic rap battle <br>
-                        It's... instupituous!!`)
+         let text = document.createTextNode(
+            `Be there
+            For the rap battle!
+            It's going to be
+            Interspectacular
+            Fantabulous
+            It'll blow your socks off!
+            It's one of those things
+            Where machine versus man
+            Man versus woman
+            Woman versus your mother
+            Be there!
+            Intergalactic rap battle
+            It's... instupituous!!`
+         )
 
          p.appendChild(text)
          p.style.opacity = "0"
